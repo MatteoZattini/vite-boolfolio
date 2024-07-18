499921a70970a95146779977f30ec10ed70ed2b5
MatteoZattini <matteozattini@hotmail.com>
2024-07-18 17:16:07 +0200

    m1+m2+m3+m4+m5 complete

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios';
+import SingleCard from './components/SingleCard.vue';
+
+
+
+export default {
+	name: 'App',
+
+	components: {
+		SingleCard,
+	},
+
+	data(){
+		return{
+			base_url: 'http://localhost:8000',
+			projects: ''
+		}
+	},
+
+	mounted(){
+		axios.get('http://localhost:8000/api/projects').then(response => {
+			console.log(response);
+			this.projects = response.data.projects.data
+			console.log(this.projects)
+		})
+	}
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<h1>ciao</h1>
+	<div class="container">
+		bs-5
+		<div class="row">
+			<div class="col-12 d-flex flex-wrap justify-content-center">
+				<template v-for="project in projects">
+					<SingleCard :card="project" />
+				</template>
+			</div>
+		</div>
+	</div>
+	
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/components/SingleCard.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    props: ["card"],
+    
+    data() {
+        return {
+            base_url: 'http://localhost:8000',
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card" style="width: calc(100% / 3);">
+        <template v-if="!card.img.startsWith('http')">
+            <img class="card-img-top" :src="base_url + '/storage/' + card.img" alt="">
+
+        </template>
+        <template v-else>
+            <img class="card-img-top" :src="card.img" alt="">
+        </template>
+        <div class="card-body">
+            {{ card.title }}
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
